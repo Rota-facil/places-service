190b46642268b72476009795fe508fe73dc064a2
Thauã Gabriel <tagashi.dev@gmail.com>
2026-06-08 20:14:33 -0300

    feat: add email propertie to send places events

--- src/main/java/com/rota/facil/places_service/menssaging/dto/send/BoardPointEvent.java
+++ src/main/java/com/rota/facil/places_service/menssaging/dto/send/BoardPointEvent.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 public record BoardPointEvent(
         UUID userId,
         Role role,
+        String email,
         String actionTitle,
         ActionType actionType,
         ResourceName resourceName,
--- src/main/java/com/rota/facil/places_service/menssaging/dto/send/InstitutionEvent.java
+++ src/main/java/com/rota/facil/places_service/menssaging/dto/send/InstitutionEvent.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 public record InstitutionEvent(
         UUID userId,
         Role role,
+        String email,
         String actionTitle,
         ActionType actionType,
         ResourceName resourceName,
